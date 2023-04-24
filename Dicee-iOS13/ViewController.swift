740e328cf252eebfd7ca926601160fcdf60560fe
priscilla <priscillakung@gmail.com>
2023-04-24 13:55:06 -0700

    made roll button to respond to user interaction

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -12,18 +12,22 @@ class ViewController: UIViewController {
     // IBOutlet allows me to reference a UI element
     // press Control and click on element to drag to code editor
     @IBOutlet weak var diceImageView1: UIImageView!
-    
     @IBOutlet weak var diceImageView2: UIImageView!
     
     override func viewDidLoad() { //code block inside curly braces
         super.viewDidLoad()
         // WHO          WHAT    VALUE
         diceImageView1.image = #imageLiteral(resourceName: "DiceSix")  //#imageliteral(
-        diceImageView1.alpha = 0.5 // half transparency
-        
+        //diceImageView1.alpha = 0.5 // half transparency
         diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
     }
 
-
+// interface builder action triggered when user touch up inside the UI element
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        print("Button got tapped.")
+        
+        diceImageView1.image = #imageLiteral(resourceName: "DiceFour")
+        diceImageView2.image = #imageLiteral(resourceName: "DiceFour")
+    }
 }
 
